f71ab80167ab80e6bbc400b0a2dd8abad2202e89
Nidhi Makhijani <nidhimj22@gmail.com>
2014-02-05 13:13:16 +0530

    updates settings.py

--- apping/settings.py
+++ apping/settings.py
@@ -107,6 +107,7 @@ ROOT_URLCONF = 'apping.urls'
 WSGI_APPLICATION = 'apping.wsgi.application'
 
 TEMPLATE_DIRS = (
+	'/home/nidhi/apping/templates'
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
